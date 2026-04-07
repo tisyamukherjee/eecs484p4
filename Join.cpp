6cc2ffbfde1b4fb982cc4ad78df12ebf584e6391
Tisya Mukherjee <tisyam@umich.edu>
2026-04-07 16:24:11 -0400

    first commit

---
+++ Join.cpp
@@ -0,0 +1,26 @@
+#include "Join.hpp"
+
+#include <vector>
+
+using namespace std;
+
+/*
+ * Input: Disk, Memory, Disk page ids for left relation, Disk page ids for right relation
+ * Output: Vector of Buckets of size (MEM_SIZE_IN_PAGE - 1) after partition
+ */
+vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
+                         pair<uint, uint> right_rel) {
+	// TODO: implement partition phase
+	vector<Bucket> partitions(0, Bucket(disk)); // placeholder
+	return partitions;
+}
+
+/*
+ * Input: Disk, Memory, Vector of Buckets after partition
+ * Output: Vector of disk page ids for join result
+ */
+vector<uint> probe(Disk* disk, Mem* mem, vector<Bucket>& partitions) {
+	// TODO: implement probe phase
+	vector<uint> disk_pages; // placeholder
+	return disk_pages;
+}
